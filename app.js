4cc0f9c7c6b2f48f8ac9236cf07301a1ad4b0b90
helmihidzir <helmihidzir@gmail.com>
2019-11-03 19:29:07 +0800

    Add prompt new grid to create a new set of grid

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let blockCount = 257;
+var blockCount = 256;
 var container = document.querySelector("#container");
 var btnClear = document.querySelector(".btnClear");
 
@@ -12,9 +12,9 @@ function getRandomColor() {
  }
 
 function generateDiv(num) {
-    for(var i = 1; i < num; i++) {
+    for(let i = 0; i < num; i++) {
       blockDiv = document.createElement("div");
-      blockDiv.className = `block`;
+      blockDiv.className = "block";
       blockDiv.style.border = "thin solid black";
       blockDiv.style.width = "40px";
       blockDiv.style.height = "40px";
@@ -25,15 +25,28 @@ function generateDiv(num) {
 generateDiv(blockCount);
 
 var allDiv = document.querySelectorAll(".block");
-
-allDiv.forEach(function(oneDiv){
-    oneDiv.addEventListener("mouseover", function(){
-        oneDiv.style.background = getRandomColor();
+function colouring(allDiv){
+    allDiv.forEach(function(oneDiv){
+        oneDiv.addEventListener("mouseover", function(){
+            oneDiv.style.background = getRandomColor();
+        })
     })
-})
+}
+
+colouring(allDiv);
 
 btnClear.addEventListener("click", function(){
     allDiv.forEach(function(oneDiv){
         oneDiv.style.background = "white";
     })
+    var newGrid = prompt("Please enter a number for a new set of grid");
+    container.innerHTML = "";
+    generateDiv(Math.pow(newGrid, 2));
+    container.style.gridTemplateColumns = "repeat("+newGrid+", 42px)";
+    container.style.gridTemplateRows = "repeat("+newGrid+", 42px)";
+    var newAllDiv = document.querySelectorAll(".block");
+    colouring(newAllDiv);
 })
+
+
+colouring(allDiv);
